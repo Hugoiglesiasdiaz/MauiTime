5581b013163e8d5b95971998a89fe91d6a65eaac
Hugoiglesiasdiaz <hugoiglesiasdiaz1@gmail.com>
2026-06-12 10:23:45 +0200

    Proyecto inicial: App de calendario en .NET MAUI

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiTime;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiTime;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
