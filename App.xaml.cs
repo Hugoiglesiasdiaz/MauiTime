98ca01a2692e70c045cc8e14a3738f700bd08173
Hugoiglesiasdiaz <hugoiglesiasdiaz1@gmail.com>
2026-06-16 10:18:50 +0200

    Servicio bd eventos montado y testeo inicial

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,29 @@
-﻿using MauiTime.Views;
-using Microsoft.Extensions.DependencyInjection;
+﻿using MauiTime;
+using MauiTime.Services;
 
 namespace MauiTime;
-
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
-	}
+    private readonly DiagnosticService _diagnostic;
 
-	protected override Window CreateWindow(IActivationState? activationState)
-	{
-		return new Window(new StartPage());
-	}
+    public App(DiagnosticService diagnostic)
+    {
+        InitializeComponent();
+        _diagnostic = diagnostic;
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        // Definimos la ventana
+        var window = new Window(new AppShell());
+
+        /*
+        // Disparamos el diagnóstico una vez creada la ventana
+        Task.Run(async () => {
+            await Task.Delay(1000); // Pequeña pausa para asegurar carga de UI
+            await _diagnostic.RunDiagnostic();
+        });*/
+
+        return window;
+    }
 }
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -11,12 +11,16 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 				fonts.AddFont("Bublegum.ttf", "Bubblegum Superstar");
 				fonts.AddFont("Bubblegum.ttf", "Bubblegum");
 				fonts.AddFont("HelveticaNowDisplay-ExtraBold.ttf", "HelveticaNowDisplay-ExtraBold");
 			});
+		builder.Services.AddSingleton<Services.DatabaseService>();
+		builder.Services.AddSingleton<Services.NotificationService>();
+		builder.Services.AddSingleton<Services.DiagnosticService>();
+		builder.Services.AddTransient<App>();
 
 #if DEBUG
 		builder.Logging.AddDebug();
